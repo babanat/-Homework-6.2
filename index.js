cc1152a519b58310ba1371253f358aa109dc8de8
babanat <babanat3@gmail.com>
2024-06-29 08:07:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let newArray = ['banana', 'apple', 'kiwi','pineapple', 34, 45, 13, 24, 11, 9, 17, 58];
 let count = 0;
- console.log(newArray.reduce((a, b) =>
+ console.log(newArray.reduce((a, b) =>  //принимает массив и сводит его к одному значению, применяя заданную функцию к каждому элементу массива последовательно.
 {
   if (typeof b == "number" && !isNaN(b)) {
     ++count;
